f4f87c39f9585f81603b9cebbe66e7d8f436df34
Windows <ivinjc@gmail.com>
2022-08-07 20:05:56 +0300

    Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -1,7 +1,9 @@
 package ru.job4j.di;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class Store {
     private List<String> data = new ArrayList<String>();
 
