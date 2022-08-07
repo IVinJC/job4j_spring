4927335bbd2a223f83e38bc9cd2d4003c1a7d04a
Windows <ivinjc@gmail.com>
2022-08-07 20:18:39 +0300

    @Autowired. Внедрение зависимости [#261276]

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -1,10 +1,12 @@
 package ru.job4j.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 @Component
 public class ConsoleInput {
+
     private Scanner scanner = new Scanner(System.in);
 
     public String askStr(String question) {
--- src/main/java/ru/job4j/di/Store.java
+++ src/main/java/ru/job4j/di/Store.java
@@ -1,4 +1,5 @@
 package ru.job4j.di;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
